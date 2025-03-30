87d4c35aacd387823d57e4376381a64017d23ecc
Phong Nguyen <nnphong1904@gmail.com>
2025-03-30 22:09:35 +0700

    setup api

--- api/index.js
+++ api/index.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const { runServerPlugin } = require("./functions");
 const app = express();
+const bodyParser = require("body-parser");
+// parse application/json
 const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
 
 // Error handling for uncaught exceptions
 process.on("uncaughtException", (err) => {
@@ -14,51 +18,15 @@ process.on("unhandledRejection", (reason, promise) => {
   process.exit(1);
 });
 
-app.get("/", async (req, res) => {
+app.post("/", async (req, res) => {
   try {
-    const code = `
-      async function fetchPageContent(url, pluginServer) {
-        const response = await fetch(
-          \`\${pluginServer}/web-page-reader/get-content?url=\${encodeURIComponent(url)}\`
-        );
-
-        if (!response.ok) {
-          throw new Error(
-            \`Failed to fetch web content: \${response.status} - \${response.statusText}\`
-          );
-        }
-        const data = await response.json();
-        return data.responseObject;
-      }
-
-      async function read_web_page_content(params, userSettings) {
-        const { url } = params;
-        const { pluginServer } = userSettings;
-        if (!pluginServer) {
-          throw new Error(
-            "Missing plugin server URL. Please set it in the plugin settings."
-          );
-        }
-
-        const cleanPluginServer = pluginServer.replace(/\\/$/, '');
-
-
-        try {
-          return await fetchPageContent(url, cleanPluginServer);
-        } catch (error) {
-          console.error("Error summarizing webpage:", error);
-          return "Error: Unable to generate a summary. Please try again later.";
-        }
-      }
-    `;
+    const { code, params, userSettings, id, name } = req.body;
     const result = await runServerPlugin({
-      id: "read_web_page_content",
-      name: "read_web_page_content",
-      code: code,
-      params: { url: "https://tamagui.dev/docs/intro/benchmarks" },
-      userSettings: {
-        pluginServer: "https://plugins-server-1o4l.onrender.com",
-      },
+      id,
+      name,
+      code,
+      params,
+      userSettings,
     });
 
     res.json({ message: "Script executed successfully in vercel", result });
